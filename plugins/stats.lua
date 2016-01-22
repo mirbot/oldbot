0549300db8679fbd2ff2e648362d40e2351cb898
mirbot <danialtkd5@gmail.com>
2016-01-22 16:32:18 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -141,8 +141,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
+    "^[!/]([Ss]tats) ([Mm]irbot)",-- Put everything you like :)
+		"^[!/](mirbot)"-- Put everything you like :)
     }, 
   run = run
 }
